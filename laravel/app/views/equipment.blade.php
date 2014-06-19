45c1eeaaff6faa4504fed1bf01dd6766e8c5b042
Noah G <nootboy2008@gmail.com>
2014-06-18 21:55:49 -0400

    download

--- laravel/app/views/equipment.blade.php
+++ laravel/app/views/equipment.blade.php
@@ -11,28 +11,13 @@ Equipment
         <div class="content">
             <h2 class="content-subhead">Our Equipment</h2>
             <p>
+            Here is a presentation of the equipment that we use<br>
+            We hope that you find it helpful<br><br>
+            <a href="{{asset('download/Equipment.ppt')}}" class="link width-wider pure-button pure-button-primary" download><span class="ion-ios7-download-outline"></span> Powerpoint Download</a>
+            <a href="{{asset('download/Equipment.pdf')}}" class="link width-wider pure-button pure-button-primary" download><span class="ion-ios7-download-outline"></span> PDF download</a>
             </p>
 
-            <div class="pure-g-r">
-                <div class="pure-u-1-4 div-space-equipment-img">
-                    <img class="equipment-display" src="ex286.jpg">
-                    <h3>EX286</h3>
-                    <p>
-                        The EX285 is top in its class from hernis
-                    </p>
-                </div>
-                <div class="pure-u-1-4 div-space-equipment-img">
-                    <img class="equipment-display" src="ex285t.jpg" alt="Train">
-                    <h3>EX285T</h3>
-                    <p>
-                        The EX285T is a versatile and durable thermal camera
-                    </p>
-                </div>
-                <div class="pure-u-1-4  div-space-equipment-img">
-                </div>
-                <div class="pure-u-1-4 div-space-equipment-img">
-                </div>
-            </div>
+            
 
         </div>
 @stop
